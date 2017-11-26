a66468dfe2ae08d57d1e47f911e8eff2a38402ab
GenieLu <tiaaait@gmail.com>
2017-11-26 04:34:30 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+// $(document).ready(function(){
+//     // Top滑道最上面
+//     $("#gototop").click(function () {
+//         $("html,body").animate({
+//             scrollTop: 0
+//         }, 1000);
+//     });
+    
+//     //錨點慢慢滾動
+//     smoothScroll.init({
+//         selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+//         selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+//         "speed": 1000
+//     });
+
+//     //啟動wow.js
+//     new WOW().init();
+
+    
+
+// });
